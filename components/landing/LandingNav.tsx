827922320d457cd7a298162a7953defff8dfe14a
Sam Liu <hello@samliu.dev>
2025-05-07 23:44:10 -0400

    Remove nav scroll

--- components/landing/LandingNav.tsx
+++ components/landing/LandingNav.tsx
@@ -1,30 +1,10 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function LandingNav() {
-  const [scrollY, setScrollY] = useState(0);
-
-  useEffect(() => {
-    const onScroll = () => {
-      setScrollY(window.pageYOffset);
-    };
-
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  });
-
   return (
-    <nav
-      className={`fixed top-0 w-full p-4 px-8 flex justify-center bg-white ${
-        scrollY > 0 ? "shadow-xs border-b" : ""
-      }`}
-    >
-      <div className="max-w-7xl grow ">
+    <nav className="fixed top-0 w-full p-4 px-8 flex justify-center bg-white">
+      <div className="max-w-7xl grow">
         <div className="flex items-center w-full">
           <h1 className="font-semibold text-2xl grow">Pager</h1>
           <div className="space-x-4">
