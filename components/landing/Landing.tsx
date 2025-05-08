5b6f79423637c26c4256e5746fadf6dcd14856d4
Sam Liu <hello@samliu.dev>
2025-05-07 23:34:02 -0400

    Responsive for mobile

--- components/landing/Landing.tsx
+++ components/landing/Landing.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image, { getImageProps } from "next/image";
 import { Button } from "@/components/ui/button";
 
 import LandingNav from "./LandingNav";
@@ -10,28 +9,31 @@ export default function Landing() {
     <>
       <LandingNav />
       <div className="min-h-screen w-full flex justify-center">
-        <div className="grid grid-cols-2 w-full max-w-6xl ">
-          <div className="flex flex-col items-left justify-center p-6 space-y-6">
-            <div className="flex">
+        <div className="grid lg:grid-cols-3 w-full max-w-6xl">
+          <div className="flex flex-col lg:col-span-2 items-left justify-center p-12 lg:p-6 space-y-4 sm:space-y-6 ">
+            <div className="flex text-sm sm:text-base">
               <p>🚧 Pager is currently under construction.</p>
             </div>
             <div className="space-y-4">
-              <h2 className="font-semibold text-5xl leading-tight">
-                Reach people quickly, without fuss.
+              <h2 className="font-semibold text-2xl sm:text-5xl leading-tight">
+                Reach people quickly,
+                <br />
+                without fuss.
               </h2>
-              <p className="text-lg text-muted-foreground">
+              <p className="text-base sm:text-lg text-muted-foreground">
                 Keep your team on the same page, update your event attendees.
+                <br />
                 Let us worry about delivering your message.
               </p>
             </div>
-            <div className="flex">
+            <div className="flex mt-8 sm:mt-0">
               <Link href="/auth/signup">
                 <Button className="text-lg px-8 py-5">Get started</Button>
               </Link>
             </div>
           </div>
           <div
-            className="w-full h-full bg-origin-content bg-contain bg-no-repeat bg-center p-30"
+            className="w-full h-full bg-origin-content bg-contain bg-no-repeat bg-center p-18 pl-0 hidden lg:block"
             style={{ backgroundImage: `url(${graphicImg.src})` }}
           ></div>
         </div>
