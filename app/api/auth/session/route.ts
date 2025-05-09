3daa56a4da68bee827532fd1cee41ad6cb89b38d
Sam Liu <hello@samliu.dev>
2025-05-09 00:23:02 -0400

    Check time in auth

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -13,6 +13,24 @@ export async function POST(request: NextRequest) {
     });
   }
 
+  // Prevent replay attacks of idToken, can only continue within 5 minutes of being issued.
+  try {
+    const decodedIdToken = await authAdmin.verifyIdToken(idToken);
+    if (new Date().getTime() / 1000 - decodedIdToken.auth_time > 5 * 60) {
+      return new Response(
+        JSON.stringify({ message: "Recent sign in required!" }),
+        {
+          status: 401,
+        }
+      );
+    }
+  } catch (error) {
+    console.error("Error verifying ID token:", error);
+    return new Response(JSON.stringify({ message: "Unauthorized" }), {
+      status: 401,
+    });
+  }
+
   const expiresIn =
     60 * 60 * 24 * (Number(process.env.AUTH_SESSION_DURATION_DAYS) ?? 7);
 
